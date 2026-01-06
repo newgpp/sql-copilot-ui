a257d59dfbf2f99f31c031692e7457e56b07a763
felix <newgpp@hotmail.com>
2026-01-06 22:53:03 +0800

    feat: scaffold vue3 + vite + ts for sql-copilot-ui

---
+++ src/pages/ChatPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="layout">
+    <div class="left">Chat Area</div>
+    <div class="right">SQL Area</div>
+  </div>
+</template>
+
+<style scoped>
+.layout {
+  display: grid;
+  grid-template-columns: 1.2fr 0.8fr;
+  height: 100%;
+}
+.left, .right {
+  border: 1px solid #eee;
+}
+</style>
